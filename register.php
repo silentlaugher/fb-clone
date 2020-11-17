fe7c2ad9224fa0872ad2ce0b7ed544e60ba1447d
Christopher Edynak <christopher1edynak@gmail.com>
2020-11-17 14:24:32 -0500

    implemented dynamic date with js

--- register.php
+++ register.php
@@ -46,6 +46,35 @@
         </form>
     </div>
 </div>
+<script src="assets/js/jquery.js"></script>
+<script>
+        for (i = new Date().getFullYear(); i > 1899; i--) {
+            //    2019,2018, 2017,2016.....1901
+            $("#years").append($('<option/>').val(i).html(i));
+
+        }
+        for (i = 1; i < 13; i++) {
+            $('#months').append($('<option/>').val(i).html(i));
+        }
+        updateNumberOfDays();
+
+        function updateNumberOfDays() {
+            $('#days').html('');
+            month = $('#months').val();
+            year = $('#years').val();
+            days = daysInMonth(month, year);
+            for (i = 1; i < days + 1; i++) {
+                $('#days').append($('<option/>').val(i).html(i));
+            }
+
+        }
+        $('#years, #months').on('change', function() {
+            updateNumberOfDays();
+        });
+        function daysInMonth(month, year) {
+            return new Date(year, month, 0).getDate();
+        }
+</script>
 <?php 
     include_once 'includes/partials/footers.php';
 ?>
