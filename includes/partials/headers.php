ccd0e7b2377e554b75a51c835667728d4ae1fa48
Christopher Edynak <christopher1edynak@gmail.com>
2020-11-17 13:14:29 -0500

    initialized app and created file structure

---
+++ includes/partials/headers.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <meta name="description" content="">
+   <meta name="author" content="Christopher Edynak">
+   <meta name="generator" content="Jekyll v4.1.1">
+   <!--custom page title -->
+   <title><?php if(isset($page_title)) echo $page_title; ?></title>
+   <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
