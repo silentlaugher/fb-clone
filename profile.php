335d48ca75c1efd235761d78b6cdf81e26e5548a
Christopher Edynak <christopher1edynak@gmail.com>
2020-11-18 16:12:48 -0500

    implemented profile html structure

---
+++ profile.php
@@ -0,0 +1,24 @@
+<?php 
+    $page_title = "Friendbook - Profile Page -";
+    include_once 'includes/partials/headers.php';
+    echo 'This is the profile page!!!';
+?>
+    <header></header>
+    <main>
+        <div class="main-area">
+            <div class="profile-left-wrap">
+                <div class="profile-cover-wrap"></div>
+                <div class="cover-bottom-part"></div>
+                <div class="bio-timeline">
+                    <div class="bio-wrap"></div>
+                    <div class="status-timeline-wrap"></div>
+                </div>
+            </div>
+            <div class="profile-right-wrap"> </div>
+        </div>
+        <div class="top-box-show"></div>
+        <div id="adv_dem"> </div>
+    </main>
+<?php 
+    include_once 'includes/partials/footers.php';
+?>
