ca6e7b55c9c78d78f1707298a388ce70daf4d425
Christopher Edynak <christopher1edynak@gmail.com>
2020-11-18 17:06:24 -0500

    implemented styling for topbar left side

--- profile.php
+++ profile.php
@@ -1,9 +1,27 @@
 <?php 
     $page_title = "Friendbook - Profile Page -";
     include_once 'includes/partials/headers.php';
-    echo 'This is the profile page!!!';
 ?>
-    <header></header>
+    <header>
+    <div class="top-bar">
+        <div class="top-left-part">
+            <div class="profile-logo"><img src="assets/img/logo.jpg" alt=""></div>
+            <div class="search-wrap">
+                <div class="search-input" style="display:flex;justify-content:center;align-items:center;width:100%;">
+                    <input type="text" name="main-search" id="main-search">
+                    <div class="s-icon top-icon top-css">
+                        <img src="assets/img/icons8-search-36.png" alt="">
+                </div>
+            </div>
+            <div id="search-show">
+                <div class="search-result">
+                </div>
+            </div>
+        </div>
+    </div>
+        <div class="top-right-part"></div>
+        </div>
+    </header>
     <main>
         <div class="main-area">
             <div class="profile-left-wrap">
