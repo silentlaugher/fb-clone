8db346d75c031f59cf8b6729d44e4766b1e9cef9
Christopher Edynak <christopher1edynak@gmail.com>
2020-11-21 14:56:47 -0500

    implemented method to send profile pic to db and root user folder

---
+++ test.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="top-box align-vertical-middle profile-dialoge-show">
+        <div class="profile-pic-upload-action">
+            <div class="pro-pic-up">
+                <div class="file-upload">
+                    <label for="profile-upload" class="file-upload-label">
+                        <snap class="upload-plus-text align-middle"><snap class="upload-plus-sign">+</snap>Upload Photo</snap>
+                    </label>
+                    <input type="file" name="file-upload" id="profile-upload" class="file-upload-input">
+                </div>
+            </div>
+            <div class="pro-pic-choose">
+                
+            </div>
+        </div>
+    </div>
+</body>
+</html>
